5490eede3be0a4708c6bc6663d46ce88e8a003fd
Poonam <maksura.alam@gmail.com>
2019-06-18 17:35:01 +0700

    Added first picture

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 
-app.get('/profile', (req, res) => {
-  res.send('Hi')
-})
+// app.get('/profile', (req, res) => {
+//   res.send('Hi')
+// })
+//
+// app.get('/first', (req, res) => {
+//   res.send('Hi, how are you doing?')
+// })
 
+app.get('/profile', (req, res) => {
+  res.sendfile('./profile.html')
+})
 
 app.listen(3000, ()=>{
   console.log('server listening to port 3000')
